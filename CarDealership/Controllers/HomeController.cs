65df244e4097b0b0de3721ce16a7129f7545621f
Shanen Cross <shanencross@yahoo.com>
2021-04-26 20:14:39 -0700

    Add route for Home Controller

--- CarDealership/Controllers/HomeController.cs
+++ CarDealership/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
+
 namespace CarDealership.Controllers
 {
   public class HomeController : Controller
   {
-
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
   }
 }
