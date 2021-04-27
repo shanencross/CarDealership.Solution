7f1145670895575101529d356e4e0a204ab16f74
Shanen Cross <shanencross@yahoo.com>
2021-04-26 20:11:34 -0700

    Fill out Car class
    Co-authored-by: Rekjal <Rekjal@users.noreply.github.com>

--- CarDealership/Models/Car.cs
+++ CarDealership/Models/Car.cs
@@ -1,7 +1,27 @@
+using System.Collections.Generic;
+
 namespace CarDealership.Models
 {
   public class Car
   {
-    
+    public string MakeModel { get; set; }
+    public int Price { get; set; }
+    public int Miles { get; set; }
+
+    public static List<Car> _instances = new List<Car> {};
+
+    public Car(string makeModel, int price, int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+
+      _instances.Add(this);
+    }
+
+    public static List<Car> GetAll()
+    {
+      return _instances;
+    }
   }
 }
