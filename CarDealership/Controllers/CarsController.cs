26442277c1546dc0ebe8d41c4c95c691961b54a6
Shanen Cross <shanencross@yahoo.com>
2021-04-26 20:14:25 -0700

    Add routes for cars controller
    Co-authored-by: Rekjal <Rekjal@users.noreply.github.com>

--- CarDealership/Controllers/CarsController.cs
+++ CarDealership/Controllers/CarsController.cs
@@ -1,9 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using CarDealership.Models;
 
-namespace CarDealership.Controllers
-{
-  public class CarsController : Controller
-  {
+namespace CarDealership.Controllers {
+  public class CarsController : Controller {
+    [HttpGet("/cars")]
+    public ActionResult Index() {
+      List<Car> allCars = Car.GetAll();
+      return View(allCars);
+
+    }
+
+    [HttpGet("/cars/new")]
+    public ActionResult CreateForm() {
+      return View();
+    }
+
+    [HttpPost("/cars")]
+    public ActionResult Create(string makemodel, int price, int miles) {
+      Car myCar = new Car(makemodel, price, miles);
+      return RedirectToAction("Index");
+    }
 
   }
 }
